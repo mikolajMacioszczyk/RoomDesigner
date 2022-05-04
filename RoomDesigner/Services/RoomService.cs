f40189c5b682795610caf79650789d0fd0056b4c
Mikołaj Macioszczyk <macioszczyk@vialutions.com>
2022-05-04 10:39:23 +0200

    combine path

--- RoomDesigner/Services/RoomService.cs
+++ RoomDesigner/Services/RoomService.cs
@@ -7,7 +7,11 @@ namespace RoomDesigner.Services
     {
         public string GetRoomFileName(WallColor color, TvSize tvSize, DecorationType decoration, CarpetType carpet)
         {
-            return string.Empty;
+            var colorName = Enum.GetName(typeof(WallColor), color);
+            var tvSizeName = Enum.GetName(typeof(TvSize), tvSize);
+            var decorationName = Enum.GetName(typeof(DecorationType), decoration);
+            var carpetName = Enum.GetName(typeof(CarpetType), carpet);
+            return Path.Combine(colorName, tvSizeName, decorationName, $"{colorName}_{tvSizeName}_{decorationName}_{carpetName}.jpg");
         }
     }
 }
